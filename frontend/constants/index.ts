ce01a292f2dd25911120876b7b1dab1539e1d9d2
Sajal Rajhans <sajalrajhans@gmail.com>
2026-01-01 02:55:06 +0530

    Update API and Cloudinary constants to use environment variables

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 
-export const API_URL = Platform.OS === "android" ? "http://192.168.29.253:3000" : "http://192.168.29.253:3000";
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
-export const CLOUDINARY_CLOUD_NAME = "dhck3lyc7";
-export const CLOUDINARY_CLOUD_PRESET = "images";
+export const CLOUDINARY_CLOUD_NAME = process.env.EXPO_PUBLIC_CLOUDINARY_NAME;
+export const CLOUDINARY_CLOUD_PRESET = process.env.EXPO_PUBLIC_CLOUDINARY_PRESET;
